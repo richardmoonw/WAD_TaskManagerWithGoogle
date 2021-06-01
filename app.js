fd787236e5ccd57b3a20a1bb994d84798958751f
Ricardo Luna <r_luna99@hotmail.com>
2021-05-31 19:15:26 -0600

    ExpressJS app configured

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
 var createError = require('http-errors');
+var cors = require('cors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var authRouter = require('./routes/auth');
+var projectRouter = require('./routes/projects');
+var ticketsRouter = require('./routes/tickets');
 
 var app = express();
 
@@ -14,15 +17,21 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/api/v1', authRouter);
+app.use('/projects', projectRouter);
+app.use('/projects/:project_id/tickets', ticketsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,5 +48,13 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+//db
+mongoose.connect('mongodb://localhost:27017/loopy', {
+  useNewUrlParser: true, 
+  useCreateIndex: true, 
+  useFindAndModify: true, 
+}).then(() => console.log('DB Connected'))
+.catch(error => console.log('Error:', error));
 
 module.exports = app;
