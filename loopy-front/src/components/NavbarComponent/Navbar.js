6aea36158b8f9995893f11aec68b90ba2b3c3d19
Ricardo Luna <r_luna99@hotmail.com>
2021-06-04 02:07:04 -0600

    Documentation added to the Navbar component

--- loopy-front/src/components/NavbarComponent/Navbar.js
+++ loopy-front/src/components/NavbarComponent/Navbar.js
@@ -10,6 +10,21 @@ import Logo from '../../assets/logo.webp';
 
 const Navbar = ({ userId, module }) => {
 
+  // Variable declarations to handle the state within the component
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  // Functions used to determine whether the app is run on a device with a large or
+  // small screen
+  const isDesktopOrLaptop = useMediaQuery({
+    minDeviceWidth: 1366
+  });
+
+  const isTabletOrSmartphone = useMediaQuery({
+    maxDeviceWidth: 1365
+  });
+
+  // Function used to handle a successful logout request
   const handleLogout = async googleData => {
     const res = await fetch("/api/v1/auth/google/logout", {
       method: "DELETE",
@@ -20,17 +35,8 @@ const Navbar = ({ userId, module }) => {
     handleClose();
   }
 
-  const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl);
-
-  const isDesktopOrLaptop = useMediaQuery({
-    minDeviceWidth: 1366
-  });
-
-  const isTabletOrSmartphone = useMediaQuery({
-    maxDeviceWidth: 1365
-  });
-
+  // Functions used to handle the different events triggered by diverse elements of the
+  // component
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
@@ -41,81 +47,84 @@ const Navbar = ({ userId, module }) => {
   
   return(
     <>
+      {/* The app is being run on a device with a large screen */}
       { isDesktopOrLaptop && 
-        <Grid className="navbarContainer" container>
-          {/* Empty space at the left */}
-          <Grid item md={1}></Grid>
-
-          {/* Brand container */}
-          <Grid item md={11}>
-            <Grid className="topPanel" container>
-              <Grid item md={1}>
-                <img className="logo" src={Logo} alt="loopy"></img>
-              </Grid>
-              <Grid item md={5}></Grid>
-              <Grid className="buttonContainer" item md={5}>
-                { module === "tasks" &&
-                    <Link 
-                      className="formattedLink" 
-                      to={{ pathname: "/projects", state: { userId: userId } }}>
-                      <Button
-                        className="navbarButton" 
-                        variant="contained"
-                        color="primary"
-                        startIcon={<AiOutlineAppstore className="buttonIcon" />}
-                        >Projects</Button>
-                    </Link>
-                }
-                <Link className="formattedLink" to="/">
-                  <Button 
-                    className="navbarButton" 
-                    variant="contained"
-                    color="secondary"
-                    onClick={() => handleLogout()}
-                    startIcon={<FiLogOut className="buttonIcon" />}
-                    >Sign Out</Button>
-                </Link>
-              </Grid>
+      <Grid className="navbarContainer" container>
+        
+        {/* Empty space at the left */}
+        <Grid item md={1}></Grid>
+        <Grid item md={11}>
+          <Grid className="topPanel" container>
+            <Grid item md={1}>
+              <img className="logo" src={Logo} alt="loopy"></img>
+            </Grid>
+            <Grid item md={5}></Grid>
+            <Grid className="buttonContainer" item md={5}>
+              { module === "tasks" &&
+              <Link 
+                className="formattedLink" 
+                to={{ pathname: "/projects", state: { userId: userId } }}>
+                <Button
+                  className="navbarButton" 
+                  variant="contained"
+                  color="primary"
+                  startIcon={<AiOutlineAppstore className="buttonIcon" />}
+                  >Projects</Button>
+              </Link>
+              }
+              <Link className="formattedLink" to="/">
+                <Button 
+                  className="navbarButton" 
+                  variant="contained"
+                  color="secondary"
+                  onClick={() => handleLogout()}
+                  startIcon={<FiLogOut className="buttonIcon" />}
+                  >Sign Out</Button>
+              </Link>
             </Grid>
           </Grid>
         </Grid>
+      </Grid>
       }
 
+      {/* The app is being run on a device with a small screen */}
       { isTabletOrSmartphone &&
-        <Grid className="mobileNavbarContainer" container>
-          <Grid item xs={3}>
-            <img className="logo" src={Logo} alt="loopy"></img>
-          </Grid>
-          <Grid item xs={8}></Grid>
-          <Grid className="routesContainer" item xs={1}>
-            <IconButton
-              onClick={handleClick}
-            >
-              <BiMenu />
-            </IconButton>
-            <Menu
-              open={open}
-              anchorEl={anchorEl}
-              keepMounted
-              onClose={handleClose}
-            >
-              <MenuItem onClick={handleClose}>
-                { module === "tasks" &&
-                <Link className="mobileFormattedLink" to="/projects">
-                  <AiOutlineAppstore className="mobileRouteIcon"/>
-                  Projects
-                </Link>
-                }
-              </MenuItem>
-              <MenuItem onClick={() => handleLogout()}>
-                <Link className="mobileFormattedLink" to="/">
-                <FiLogOut className="mobileRouteIcon"/>
-                 Sign Out
-                </Link>
-              </MenuItem>
-            </Menu>
-          </Grid>
+      <Grid className="mobileNavbarContainer" container>
+        <Grid item xs={3}>
+          <img className="logo" src={Logo} alt="loopy"></img>
         </Grid>
+        <Grid item xs={8}></Grid>
+        <Grid className="routesContainer" item xs={1}>
+          <IconButton
+            onClick={handleClick}
+          >
+            <BiMenu />
+          </IconButton>
+          <Menu
+            open={open}
+            anchorEl={anchorEl}
+            keepMounted
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>
+              { module === "tasks" &&
+              <Link 
+                className="formattedLink" 
+                to={{ pathname: "/projects", state: { userId: userId } }}>
+                <AiOutlineAppstore className="mobileRouteIcon"/>
+                Projects
+              </Link>
+              }
+            </MenuItem>
+            <MenuItem onClick={() => handleLogout()}>
+              <Link className="mobileFormattedLink" to="/">
+              <FiLogOut className="mobileRouteIcon"/>
+                Sign Out
+              </Link>
+            </MenuItem>
+          </Menu>
+        </Grid>
+      </Grid>
       }
     </>
   );
