b7a703b028e25809c4a7a68d49f911b2d336f12c
Ricardo Luna <r_luna99@hotmail.com>
2021-06-05 00:53:56 -0600

    Documentation added to the projects' controller

--- routes/projects.js
+++ routes/projects.js
@@ -2,24 +2,27 @@ var express = require('express');
 var router = express.Router();
 const Project = require('../models/project');
 
+// API endpoints generated for the Projects controller. The functions within this controller
+// allows you to perform CRUD operations over the Project model
+
+// Get all the projects' data for a given user.
 router.get("/", async (req, res) => {
   projects = await Project.find({userId: req.query.userId }).exec();
-
   res.json(projects)
 })
 
+// Get a specific project data given its unique identifier.
 router.get("/:id", async(req, res) => {
   project = await Project.findOne({_id: req.params.id}).exec()
-
   res.json(project)
 }) 
 
+// Store a new project in the database. The project must be linked to a specific user.
 router.post("/", async (req, res) => {
   try {
     const { project } = req.body;
     const newProject = await new Project({name: project.name, description: project.description,
                                       start_at: project.start_at, userId: project.userId}).save()
-    
     res.json(newProject);
   }
   catch(error) {
@@ -28,6 +31,7 @@ router.post("/", async (req, res) => {
   }
 }) 
 
+// Update an existing project in the database. 
 router.put("/:id", async (req, res) => {
   try {
     const { project } = req.body;
@@ -50,6 +54,7 @@ router.put("/:id", async (req, res) => {
   }
 })
 
+// Delete an existing project and all its associated tickets from the database.
 router.delete("/:id", async(req, res) => {
   try {
     const deletedProject = await Project.findOneAndDeleteAll({
