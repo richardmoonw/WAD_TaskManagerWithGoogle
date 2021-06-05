9243932db6418a4d3032350ee9080f7a4dced6bb
Ricardo Luna <r_luna99@hotmail.com>
2021-06-05 00:48:45 -0600

    Documentation added to the authentication controller

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,11 @@ const User = require('../models/user');
 const { OAuth2Client } = require('google-auth-library');
 const client = new OAuth2Client("868449691994-qs07uckaki4h1r630hphiliaua6ucuq0.apps.googleusercontent.com")
 
+// API endpoints generated for the Authentication controller. The functions within this controller
+// allows you to authenticate a user with its Google credentials, and finish the session for a given 
+// user.
+
+// Authenticate a user to use the application with their Google credentials.
 router.post("/auth/google", async (req, res) => {
   const { token }  = req.body
   const ticket = await client.verifyIdToken({
@@ -11,12 +16,15 @@ router.post("/auth/google", async (req, res) => {
       audience: "868449691994-qs07uckaki4h1r630hphiliaua6ucuq0.apps.googleusercontent.com"
   });
 
+  // Get the basic data provided by Google for the requested user.
   const { sub, name, email } = ticket.getPayload();    
 
+  // If the user already exists in the database, retrieve their information.
   var user = await User.findOne(
     { id: sub }, { name, email }
   )
-
+  
+  // If the user does not exist in the database, create it.
   if(!user) {
     user = await new User({
       id: sub,
@@ -25,12 +33,13 @@ router.post("/auth/google", async (req, res) => {
     }).save()
   }
 
+  // Send the retrieved data to the client.
   req.session.userId = user.id
-
   res.status(201)
   res.json(user)
 })
 
+// Finish the session for a given user.
 router.delete("/auth/google/logout", async(req, res) => {
   await req.session.destroy()
   res.status(200)
