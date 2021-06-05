565bce7fac507aa7892416b014bcd7ea95ed3157
Ricardo Luna <r_luna99@hotmail.com>
2021-06-05 01:01:35 -0600

    Documentation added to the app.js file of Express.js

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var mongoose = require('mongoose')
+var app = express();
 
+// Import the contollers for the different available entities.
 var authRouter = require('./routes/auth');
 var projectRouter = require('./routes/projects');
 var ticketsRouter = require('./routes/tickets');
 
-var app = express();
-
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -29,27 +29,29 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Map the parent routes to the imported controllers of the different available
+// entities
 app.use('/api/v1', authRouter);
 app.use('/projects', projectRouter);
 app.use('/projects/:project_id/tickets', ticketsRouter);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
-//db
+// Set the database connection
 mongoose.connect('mongodb://localhost:27017/loopy', {
   useNewUrlParser: true, 
   useCreateIndex: true, 
